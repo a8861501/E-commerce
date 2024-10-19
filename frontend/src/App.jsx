63bf0336ea66d450454dc45f7a106d9ea3887a12
a8861501 <a8861501@gmail.com>
2024-10-19 23:07:45 +0800

    Add Navbar Add search icon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Collection from './pages/Collection'
+import Product from './pages/Product'
+import Contact from './pages/Contact'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
+
 
 function App() {
   return (
-    <div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/contact'element={<Contact />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login'element={<Login />} />
+        <Route path='/plcae-order' element={<PlaceOrder/>}/>
+        <Route path='/orders' element={<Orders/>}/>
+      </Routes>
       
     </div>
   )
